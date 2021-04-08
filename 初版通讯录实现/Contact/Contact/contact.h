18f297812885984163fa9b7675a6c8109f170f8f
FYL1710905839 <1710905839@qq.com>
2021-04-08 23:27:09 +0800

    这是我入门后的练习小成果

---
+++ 初版通讯录实现/Contact/Contact/contact.h
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 1000
+#define MAX_NAME 20
+#define MAX_SEX 5
+#define MAX_TELE 12
+#define MAX_ADDR 30
+
+enum Option
+{
+	EXIT,//0
+	ADD,//1
+	DEL,
+	SEARCH,
+	MONDIFY,
+	SHOW,
+	SORT
+};
+
+struct PeoInfo
+{
+	char name[MAX_NAME];
+	int age;
+	char sex[MAX_SEX];
+	char tele[MAX_TELE];
+	char addr[MAX_ADDR];
+};
+
+//通讯录类型
+struct Contact
+{
+	struct PeoInfo data[MAX];//存放1000个信息
+	int size;//记录当前已经有的元素个数
+};
+
+//声明函数
+//初始化通讯录的函数
+void InitContact(struct Contact* ps);
+
+//增加一个信息到通讯录
+void AddContact(struct Contact* ps);
+
+//打印通讯录中的信息
+void ShowContact(const struct Contact* ps);
+
+//删除指定的联系人
+void DelContact(struct Contact* ps);
+
+//查找指定的人的信息
+void SearchContact(const struct Contact* ps);
+
+//修改指定的联系人
+void ModifyContact(struct Contact* ps);
+
+//排序通讯录内容
+SortContact(struct Contact* ps);
---
+++ 初版通讯录实现/Contact/Contact/test.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "contact.h"
+
+void menu()
+{
+	printf("*******************************************\n");
+	printf("**********  1.add      2.del     **********\n");
+	printf("**********  3.search   4.modify  **********\n");
+	printf("**********  5.show     6.sort    **********\n");
+	printf("**********  0.exit               **********\n");
+	printf("*******************************************\n");
+}
+
+int main()
+{
+	int input = 0;
+	//创建通讯录
+	struct Contact con;//con就是通讯录，里面包含：1000的元素的数和size
+	//初始化通讯录
+	InitContact(&con);
+	//
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case DEL:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;
+		case MONDIFY:
+			ModifyContact(&con);
+			break;
+		case SHOW:
+			ShowContact(&con);
+			break;
+		case SORT:
+			SortContact(&con);
+			break;
+		case EXIT:
+			printf("退出通信录\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
