7b6ee00abf88d896dd307bfae706db42087d4986
灰级计萌 <78867289+YISHENGPINGAN51@users.noreply.github.com>
2021-04-13 13:06:33 +0800

    这是我入门后的练习小成果

---
+++ 动态版通讯录实现/Contact_Dynamic/Contact_Dynamic/test.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "contact.h"
+
+void menu()
+{
+	printf("*******************************************\n");
+	printf("**********  1.add      2.del     **********\n");
+	printf("**********  3.search   4.modify  **********\n");
+	printf("**********  5.show     6.sort    **********\n");
+	printf("**********  0.exit               **********\n");
+	printf("*******************************************\n");
+}
+
+int main()
+{
+	int input = 0;
+	//创建通讯录
+	struct Contact con;//con就是通讯录，里面包含：data指针, size, capacity
+	//初始化通讯录
+	InitContact(&con);
+	//
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case DEL:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;
+		case MONDIFY:
+			ModifyContact(&con);
+			break;
+		case SHOW:
+			ShowContact(&con);
+			break;
+		case SORT:
+			SortContact(&con);
+			break;
+		case EXIT:
+			//销毁通讯录-释放动态开辟的内存
+			DestroyContact(&con);
+			printf("退出通信录\n");
+			break;
+		default:
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
